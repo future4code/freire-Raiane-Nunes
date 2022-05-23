1fd05f3e671210d27bf589a3508b03fe54e1d8ff
Diego Guimarães <diego.guimaraes.si@gmail.com>
2022-05-23 13:24:00 -0300

    array invertido

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -10,7 +10,11 @@ function retornaTamanhoArray(array) {
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-
+    let arrayInvertido = [];
+    for (let i = 0; i < array.length; i++) {
+        arrayInvertido[i] = array[array.length - i - 1];
+    }
+    return arrayInvertido;
 }
 
 // EXERCÍCIO 03
