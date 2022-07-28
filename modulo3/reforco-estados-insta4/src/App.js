4a9032c3d0056eb5c2b50b7b565756d03cccb7f0
Diego Guimarães <diego.guimaraes.si@gmail.com>
2022-07-27 22:23:35 -0300

    reforço insta 4

---
+++ modulo3/reforco-estados-insta4/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import Post from "./components/Post/Post";
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+function App() {
+  return (
+    <MainContainer>
+      <Post
+        nomeUsuario={"Paulinha"}
+        fotoUsuario={"https://picsum.photos/50/50?random=1"}
+        fotoPost={"https://picsum.photos/200/150?random=2"}
+      />
+      <Post
+        nomeUsuario={"Bia"}
+        fotoUsuario={"https://picsum.photos/50/50?random=3"}
+        fotoPost={"https://picsum.photos/200/150?random=4"}
+      /><Post
+        nomeUsuario={"Isadora"}
+        fotoUsuario={"https://picsum.photos/50/50?random=5"}
+        fotoPost={"https://picsum.photos/200/150?random=6"}
+      />
+    </MainContainer>
+  );
+}
+
+export default App;
